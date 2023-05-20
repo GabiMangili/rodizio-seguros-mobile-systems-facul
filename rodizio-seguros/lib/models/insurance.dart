38d4d6655ec8380a8841210193a3d870aeb396dc
Gabi Mangili <gabiimangiilii@gmail.com>
2023-05-20 15:16:45 -0300

    fazendo modelos das classes diver e vehicle

--- rodizio-seguros/lib/models/insurance.dart
+++ rodizio-seguros/lib/models/insurance.dart
@@ -1,10 +1,24 @@
+import 'package:untitled/models/vehicle.dart';
+import 'diver.dart';
+
 class Insurance{
   final String status;
-  final String vehicle; //veículo
+  final String vehicle;
   final String brand; //marca
   final int year; //ano
-  final String plate; //placa
+  final String plate;
 
-  Insurance({required this.status, required this.vehicle, required this.brand, required this.year, required this.plate});
+  Insurance({required this.status, required this.vehicle, required this.brand, required this.year, required this.plate}); //placa
+
+  // final String status;
+  // final Vehicle vehicle;
+  // final Driver driver;
+  //
+  // Insurance({required this.status, required this.vehicle, required this.driver});
+
+//   Insurance.fromMap(Map map)
+//     : status = map[""],
+//       vehicle = Vehicle.fromMap(map[""]),
+//       driver = Driver.fromMap(map[""]);
 }
 
